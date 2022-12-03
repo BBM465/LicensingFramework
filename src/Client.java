ed4a720d06fe0bf74fd31d8bc5c41cc853fc7dd0
imrekosdik <imrekosdik@gmail.com>
2022-12-03 15:56:02 +0300

    signature size problem solved. Program outputs the license not found condition.

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,6 @@
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -14,69 +12,107 @@ import java.nio.file.Path;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 public class Client {
-    private final String serialNumber;
-    private final String username;
-    private final String motherboardSerialNumber;
-    private String MACAddress;
-    private final String diskSerialNumber;
+    private static String serialNumber;
+    private static String username;
+    private static String motherboardSerialNumber;
+    private static String MACAddress;
+    private static String diskSerialNumber;
     private byte[] signature;
-    private final PublicKey publicKey;
-
-
-    public Client(String motherboardSerialNumber, String diskSerialNumber) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        this.motherboardSerialNumber = motherboardSerialNumber; // os dependent - must be implemented hard-coded
-        this.diskSerialNumber = diskSerialNumber; // os dependent - must be implemented hard-coded
-        this.username = "ImreAndCagla"; // static
-        this.serialNumber = "1234-5678-9012"; // static
-        this.MACAddress = getMACAddress(); // the function to receive mac address can be implemented independent of the platform
+    private static PublicKey publicKey;
+    private static String licenseContent;
+    private static String hashLicenseContent;
+    public static LicenceManager licenceManager;
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, SignatureException {
+        System.out.println("Client started...");
+        // change the address to function call when submitting
+        MACAddress = "36:7d:da:df:54:9e"; // the function to receive mac address can be implemented independent of the platform
+        System.out.println("My MAC: " + MACAddress);
+        diskSerialNumber = "C02DW0N0ML7H"; // change it to windows function call when submitting
+        System.out.println("My Disk ID: " + diskSerialNumber);
+        // 4810-E58D for Windows Machine
+        // C02DW0N0ML7H for macOS Machine
+        motherboardSerialNumber = "820-01949-A"; // hard-coded for macOS machine - change it to windows function call when submitting
+        System.out.println("My Motherboard ID: " + motherboardSerialNumber);
+        System.out.println("LicenseManager service started...");
+        username = "ImreAndCagla"; // static
+        serialNumber = "1234-5678-9012"; // static
         KeyFactory kf = KeyFactory.getInstance("RSA");
-        this.publicKey = kf.generatePublic(new PKCS8EncodedKeySpec(Files.readAllBytes(Path.of("src/keys/public.key"))));
+        publicKey = kf.generatePublic(new X509EncodedKeySpec(Files.readAllBytes(Path.of("src/keys/public.key"))));
+        File f = new File("licence.txt");
+        licenceManager=new LicenceManager();
+        if(f.exists() && !f.isDirectory()) {
+
+        }else {
+            System.out.println("Client -- License file is not found.");
+            getLicenceContent();
+            sendEncryptedLicenseContent(licenceManager);
+            byte[] managersLicence = licenceManager.getSignature();
+            if(verifyLicense(managersLicence)){
+                System.out.println("Client -- License file is not found.");
+                System.out.println("Client -- Succeeded. The license file content is secured and signed by the server.");
+            }
+        }
     }
     public void setSignature(byte[] signature) {
         this.signature = signature;
     }
 
-    private String getMACAddress() throws SocketException, UnknownHostException {
-        InetAddress localHost = InetAddress.getLocalHost(); // get address for machine's local host
+    private static String getMACAddress() throws SocketException, UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
         NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
         byte[] hardwareAddress = ni.getHardwareAddress();
         String[] hexadecimal = new String[hardwareAddress.length];
         for (int i = 0; i < hardwareAddress.length; i++) { // return the address in hexadecimal format
             hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
         }
-        this.MACAddress = String.join(":", hexadecimal); // split each number with ":" character
+        MACAddress = String.join(":", hexadecimal); // split each number with ":" character
         return MACAddress;
     }
 
-    private String getLicenceContent(){
-        return username + "$" + serialNumber + "$" + MACAddress + "$" + diskSerialNumber + "$" + motherboardSerialNumber;
+    private static String getLicenceContent(){
+        String rawLicenseText = username + "$" + serialNumber + "$" + MACAddress + "$" + diskSerialNumber + "$" + motherboardSerialNumber;
+        licenseContent = rawLicenseText;
+        System.out.println("Client -- Raw License Text: " + rawLicenseText);
+        return rawLicenseText;
     }
 
-    private String EncryptLicenseContent() throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private static byte[] EncryptLicenseContent() throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         // encrypt the content with public key
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        return new String(cipher.doFinal(getLicenceContent().getBytes()));
+        byte[] encrypted = cipher.doFinal(licenseContent.getBytes());
+        System.out.print("Client -- Encrypted License Text: ");
+        System.out.println(new String(Base64.getUrlEncoder().encode(encrypted)));
+        return encrypted;
     }
 
-    public void sendEncryptedLicenseContent(LicenceManager licenceManager) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, InvalidKeyException {
-        licenceManager.setEncryptedLicenseContent(EncryptLicenseContent());
+    public static void sendEncryptedLicenseContent(LicenceManager licenceManager) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, InvalidKeyException, SignatureException {
+        byte[] content = EncryptLicenseContent();
+        HashLicenseContent();
+        System.out.println("Server is being requested... ");
+        licenceManager.setEncryptedLicenseContent(content);
     }
 
-    private String HashLicenseContent() throws NoSuchAlgorithmException {
+    private static String HashLicenseContent() throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("MD5"); // take the hash of the license content with MD5
-        byte[] hash = digest.digest(getLicenceContent().getBytes(StandardCharsets.UTF_8));
-        return new String(Base64.getEncoder().encode(hash), StandardCharsets.UTF_8); // convert the hashed bytes to string
+        byte[] hash = digest.digest(licenseContent.getBytes(StandardCharsets.UTF_8));
+        String md5LicenseText = new String(Base64.getUrlEncoder().encode(hash), StandardCharsets.UTF_8);
+        System.out.println("Client -- MD5 License Text: " + md5LicenseText);
+        hashLicenseContent = md5LicenseText;
+        return md5LicenseText; // convert the hashed bytes to string
     }
 
-    // https://stackoverflow.com/questions/7224626/how-to-sign-string-with-private-key
-    public boolean verifyLicense() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    public static boolean verifyLicense(byte[] licenceSignature) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         Signature signature = Signature.getInstance("SHA256WithRSA");
         signature.initVerify(publicKey); // verify signature with public key
-        signature.update(HashLicenseContent().getBytes());
-        return signature.verify(this.signature);
+        signature.update(Base64.getUrlDecoder().decode(hashLicenseContent));
+        return signature.verify(licenceSignature);
     }
 }
--- src/LicenceManager.java
+++ src/LicenceManager.java
@@ -1,7 +1,4 @@
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -10,42 +7,65 @@ import java.nio.file.Path;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 public class LicenceManager {
-    private String encryptedLicenseContent;
+    private byte[] encryptedLicenseContent;
     private final PrivateKey privateKey;
+
+    private byte[] signature;
+
+    public void setSignature(byte[] signature){
+        this.signature=signature;
+    }
+
+    public byte[] getSignature() {
+        return signature;
+    }
 
     public LicenceManager() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
         KeyFactory kf = KeyFactory.getInstance("RSA");
         this.privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(Files.readAllBytes(Path.of("src/keys/private.key"))));
+
     }
-    public void setEncryptedLicenseContent(String encryptedLicenseContent) {
+    public void setEncryptedLicenseContent(byte[] encryptedLicenseContent) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, InvalidKeyException, SignatureException {
         this.encryptedLicenseContent = encryptedLicenseContent;
+        System.out.print("Server -- Incoming Encrypted License Text: ");
+        System.out.println(new String(Base64.getUrlEncoder().encode(encryptedLicenseContent)));
+        //first element in list encrypted text,second element is encrypted key
+        sendSignature();
     }
 
-    private String DecryptLicenseContent() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private byte[] DecryptLicenseContent() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         // decrypt the content with the private key
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        return new String(cipher.doFinal(encryptedLicenseContent.getBytes()));
+        byte[] decrypted = cipher.doFinal(encryptedLicenseContent);
+        String decryptedText =  new String(decrypted);
+        System.out.println("Server -- Decrypted License Text: " + decryptedText);
+        return decrypted;
     }
 
-    private String HashDecryptedLicenseContent() throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException {
+    private byte[] HashDecryptedLicenseContent() throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException {
         MessageDigest digest = MessageDigest.getInstance("MD5"); // take the hash of the decrypted license content with MD5
-        byte[] hash = digest.digest(DecryptLicenseContent().getBytes(StandardCharsets.UTF_8));
-        return new String(Base64.getEncoder().encode(hash), StandardCharsets.UTF_8); // convert the hashed bytes to string
+        byte[] hash = digest.digest(DecryptLicenseContent());
+        String hashedLicense = new String(Base64.getUrlEncoder().encode(hash), StandardCharsets.UTF_8);
+        System.out.println("Server -- MD5 Plain License Text: " + hashedLicense);
+        return hash;
     }
 
-    // https://stackoverflow.com/questions/7224626/how-to-sign-string-with-private-key
-    private String SignHashedLicenseContent() throws NoSuchAlgorithmException, IOException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, SignatureException {
+    private byte[] SignHashedLicenseContent() throws NoSuchAlgorithmException, IOException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, SignatureException {
         Signature signature = Signature.getInstance("SHA256WithRSA");
         signature.initSign(privateKey); // sign the message with private key
-        signature.update(HashDecryptedLicenseContent().getBytes()); // sign hash of the decrypted message
-        return new String(Base64.getEncoder().encode(signature.sign()));
+        signature.update(HashDecryptedLicenseContent()); // sign hash of the decrypted message
+        byte[] signed = signature.sign();
+        System.out.println("Server -- Digital Signature: " + new String(Base64.getUrlEncoder().encode(signed)));
+        return signed;
     }
 
-    public void sendSignature(Client client) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, SignatureException, InvalidKeyException {
-        client.setSignature(SignHashedLicenseContent().getBytes());
+    public void sendSignature() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, SignatureException, InvalidKeyException {
+        setSignature(SignHashedLicenseContent());
     }
 }
